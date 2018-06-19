4bc13db2bc708255e2e2519ec5db876c0954a5f0
lu <luchenwei9266@users.noreply.github.com>
2018-06-19 17:38:27 +0800

    完善币种详情的界面

--- pages/coinDetails/index.js
+++ pages/coinDetails/index.js
@@ -1,13 +1,43 @@
+const URL = 'https://api.coingecko.com/api/v3/coins/';
 
 Page({
-  data: {
-    coinObject: {}
-  },
+    data: {
+        coinObject: {},
+        coinId: ""
+    },
 
-  onShow: function () {
-    let coinObject = wx.getStorageSync("coin");
-    console.log(coinObject);
-    this.setData({coinObject});
-  }
+    onLoad: function(options) {
+        if (options.coinId) {
+            wx.showLoading({ title: '加载中' })
+            wx.request({
+                url: URL + options.coinId,
+                success: function(res) {
+                    wx.hideLoading();
+                    this.setData({ coinObject });
+                },
+                fail: function(error) {
+                    wx.hideLoading();
+                    console.log(error);
+                }
+            })
+        }
+    },
+
+    onShareAppMessage: function(res) {
+        if (res.from === 'button') {
+            // 来自页面内转发按钮
+            console.log(res.target)
+        }
+        return {
+            title: this.coinObject.localization.zh + "/" + this.coinObject.localization.en,
+            path: 'pages/coinDetails/index?coinId=' + this.coinObject.localization.id,
+            imageUrl: this.coinObject.image.small
+        }
+    },
+
+    onShow: function() {
+        let coinObject = wx.getStorageSync("coin");
+        this.setData({ coinObject });
+    }
 
 })
